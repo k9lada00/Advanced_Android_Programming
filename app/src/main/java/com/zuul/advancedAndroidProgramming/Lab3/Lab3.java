ca4f24402b4b257d1dc41ca44a5927eb365c832e
k9lada00 <60172591+k9lada00@users.noreply.github.com>
2020-04-26 16:08:16 +0300

    Started Lab_3_1

--- app/src/main/java/com/zuul/advancedAndroidProgramming/Lab3/Lab3.java
+++ app/src/main/java/com/zuul/advancedAndroidProgramming/Lab3/Lab3.java
@@ -1,7 +1,12 @@
 package com.zuul.advancedAndroidProgramming.Lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.zuul.advancedAndroidProgramming.MainActivity;
 import com.zuul.advancedAndroidProgramming.R;
 
 public class Lab3 extends AppCompatActivity
@@ -12,5 +17,29 @@ public class Lab3 extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lab_3);
+    }
+
+    public void select31(View v)
+    {
+        Intent i = new Intent (this, Lab_3_1.class);
+        startActivity(i);
+    }
+
+    public void select32(View v)
+    {
+        Intent i = new Intent (this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void select33(View v)
+    {
+        Intent i = new Intent (this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void select34(View v)
+    {
+        Intent i = new Intent (this, MainActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/zuul/advancedAndroidProgramming/Lab3/Lab_3_1.java
@@ -0,0 +1,42 @@
+package com.zuul.advancedAndroidProgramming.Lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.zuul.advancedAndroidProgramming.R;
+
+public class Lab_3_1 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout colorTitleContainer = new LinearLayout(this);
+        colorTitleContainer.setHorizontalGravity(1);
+
+        TextView colorNameTitle = new TextView(this);
+        colorNameTitle.setTextColor(getResources().getColor(android.R.color.black));
+        colorNameTitle.setPadding(60, 60, 60, 60);
+        colorNameTitle.setText("White");
+
+        colorTitleContainer.setBackgroundColor(getResources().getColor(android.R.color.white));
+        colorTitleContainer.addView(colorNameTitle);
+        setContentView(colorTitleContainer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.lab_3_1, menu);
+        return true;
+    }
+
+}
